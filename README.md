10e562ca5bb9f1d4489ff3c773c5584b9ab1b096
breelovesfrogs <134086243+breelovesfrogs@users.noreply.github.com>
2023-10-23 16:05:22 -0500

    added 'thoughtController', thank you to Greg and Kevin for helping me get started with this section!

--- README.md
+++ README.md
@@ -1,8 +1,18 @@
 # Social-Media-API
 An API than can be used as a base to help create your own social media platform!
 
+## Credits
+
+I used heavily referenced the mini project for this module, as well as Kevin's and Greg's repositories. Thank you for sharing! I struggled to get started on this project/understand mongoose, so looking at my classmates code helped me to get started, I looked up things I didn't understand and tried to add comments explaining the code where I didn't already know what the code did. Here are the links to the resources I used.
+
 https://stackoverflow.com/questions/20766360/whats-the-meaning-of-trim-when-use-in-mongoose
 
 https://masteringjs.io/tutorials/mongoose/unique
 
 https://stackoverflow.com/questions/28829912/mongoose-schema-set-max-length-for-a-string
+
+https://mongoosejs.com/docs/tutorials/findoneandupdate.html
+
+'$push / $pull' https://mongoosejs.com/docs/5.x/docs/api/array.html#mongoosearray_MongooseArray-pull
+
+'save()' https://mongoosejs.com/docs/documents.html#updating-using-save
---
+++ controllers/thoughtController.js
@@ -0,0 +1,104 @@
+const { Thought, User } = require('../Models');
+
+module.exports = {
+    // get all thoughts
+    async getThoughts(req, res) {
+        try {
+            const thoughts = await Thought.find();
+            res.json(thoughts);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+    // get a single thought
+    async getSingleThought(req, res) {
+        try{
+            const thought = await Thought.findOne({_id: req.params.id});
+            res.status(200).json(thought);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+    // create a thought
+    async createThought(req, res) {
+        try {
+            // new thought from the user input
+            const newThought = await Thought.create(req.body);
+            // finds the user that the thought will belong to and updates the user.
+            const user = await User.findOneAndUpdate(
+                { _id: req.body.userId},
+                // $push is pushing the new thought to the 'thoughts' property of user.
+                { $push: {thoughts: newThought._id} },
+                // {new: true} will return the document after it has been updated. By default, it returns
+                // the document before it is updated.
+                {new: true}
+            )
+            res.status(200).json(user)
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+    // update a thought
+    async updateThought(req, res) {
+        try {
+            const updatedThought = await Thought.findOneAndUpdate(
+                { _id: req.params.id },
+                { thoughtText: req.body.thoughtText },
+                { new: true }
+            );
+            // according to mongoosejs.com, it says that .save() is the correct way to update
+            // an existing document. .save() allows full validation and middleware.
+            await updatedThought.save();
+            res.status(200).json(updatedThought);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+    // delete a thought
+    async deleteThought(req, res) {
+        try {
+            const deletedThought = await Thought.findOneAndDelete(
+                { _id: req.params.id }
+            );
+            const user = await User.findOneAndUpdate(
+                { thoughts: req.params.id },
+                // after the thought id is found, it is 'pulled' from the array, aka deleted.
+                { $pull: {thoughts: req.params.id} },
+                { new: true }
+            );
+            await user.save()
+            res.status(200).json(deletedThought);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+    // create a reaction
+    async createReaction(req, res) {
+        try {
+            const newReaction = await Thought.findOneAndUpdate(
+                { _id: req.params.thoughtId },
+                { $push: {reactions: req.body} },
+                // validators for update functions are off by default, to enable them, we have
+                // to use 'runValidators'
+                // we are using a validator on the length of the body
+                { new: true, runValidators: true }
+            );
+            res.status(200).json(newReaction);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+    // delete a reaction
+    async deleteReaction(req, res) {
+        try {
+            const deletedReaction = await Thought.findOneAndUpdate(
+                { _id: req.params.thoughtId },
+                { $pull: {reactions: {id: req.params.id}} },
+                { new: true }
+            );
+            res.status(200).json(deletedReaction);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    }
+}
